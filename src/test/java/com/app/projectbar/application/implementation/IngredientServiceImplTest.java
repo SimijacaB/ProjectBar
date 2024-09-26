6bfd9ee4cb9054974ad1cce04e667b847faf2a24
Santiago <santiagovalenciarios@gmail.com>
2024-09-26 04:49:07 -0500

    Se agregan dependencias para pruebas unitarias (mockito, junit y jacoco), pendiente inicio de estas

---
+++ src/test/java/com/app/projectbar/application/implementation/IngredientServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.app.projectbar.application.implementation;
+
+import com.app.projectbar.infra.repositories.IIngredientRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientServiceImplTest {
+
+    @MockBean
+    private IIngredientRepository ingredientRepository;
+
+
+
+
+}
