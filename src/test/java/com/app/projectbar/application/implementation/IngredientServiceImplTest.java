e1f8b9b5b0b6d7df7d33ecf1500ae9c019fd39f1
Santiago <santiagovalenciarios@gmail.com>
2024-10-02 05:15:49 -0500

    Se prosiguen con los test en Ingredient, se realiza test de finByCode y a la espera de terminar test de finAll()

--- src/test/java/com/app/projectbar/application/implementation/IngredientServiceImplTest.java
+++ src/test/java/com/app/projectbar/application/implementation/IngredientServiceImplTest.java
@@ -1,9 +1,10 @@
 package com.app.projectbar.application.implementation;
 
 import com.app.projectbar.domain.Ingredient;
-import com.app.projectbar.domain.dto.ingredient.IngredientResponseDTO;
+import com.app.projectbar.domain.dto.ingredient.*;
 import com.app.projectbar.domain.enums.UnitOfMeasure;
 import com.app.projectbar.infra.repositories.IIngredientRepository;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,10 +13,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+
 
 class IngredientServiceImplTest {
 
@@ -32,6 +35,43 @@ class IngredientServiceImplTest {
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
+
+        Long id = 1L;
+        Ingredient ingredient1 = new Ingredient(1L, "G01-ML-0001I", "Ginebra", UnitOfMeasure.ML, null);
+        IngredientResponseDTO ingredientResponseDTO1 = new IngredientResponseDTO();
+        ingredientResponseDTO1.setId(1L);
+        ingredientResponseDTO1.setCode("G01-ML-0001I");
+        ingredientResponseDTO1.setName("Ginebra");
+        ingredientResponseDTO1.setUnitOfMeasure("ML");
+
+        when(this.ingredientRepository.findById( anyLong() )).thenReturn(Optional.of(ingredient1));
+        when(this.ingredientRepository.findByCode("G01-ML-0001I")).thenReturn(Optional.of(ingredient1));
+        when(modelMapper.map(ingredient1, IngredientResponseDTO.class)).thenReturn(ingredientResponseDTO1);
+
+        Ingredient ingredient2 = new Ingredient(2L, "T01-ML-0002I", "Tonica", UnitOfMeasure.ML, null);
+        IngredientResponseDTO ingredientResponseDTO2 = new IngredientResponseDTO();
+        ingredientResponseDTO2.setId(2L);
+        ingredientResponseDTO2.setCode("T01-ML-0002I");
+        ingredientResponseDTO2.setName("Tonica");
+        ingredientResponseDTO2.setUnitOfMeasure("ML");
+
+        when(this.ingredientRepository.findById( anyLong() )).thenReturn(Optional.of(ingredient1));
+        when(this.ingredientRepository.findByCode("T01-ML-0002I")).thenReturn(Optional.of(ingredient2));
+        when(modelMapper.map(ingredient2, IngredientResponseDTO.class)).thenReturn(ingredientResponseDTO2);
+
+
+        Ingredient ingredient3 = new Ingredient(3L, "L01-UN-0001I", "Unidad de Limon", UnitOfMeasure.ML, null);
+        IngredientResponseDTO ingredientResponseDTO3 = new IngredientResponseDTO();
+        ingredientResponseDTO3.setId(2L);
+        ingredientResponseDTO3.setCode("L01-UN-0001I");
+        ingredientResponseDTO3.setName("Unidad de Limon");
+        ingredientResponseDTO3.setUnitOfMeasure("ML");
+
+        List<Ingredient> resultList = List.of(
+                ingredient1, ingredient2, ingredient3);
+
+        when(this.ingredientRepository.findAll()).thenReturn(resultList);
+        when(modelMapper.map(ingredient2, IngredientResponseDTO.class)).thenReturn(ingredientResponseDTO2);
     }
     /*
     {
@@ -59,15 +99,6 @@ class IngredientServiceImplTest {
     public void testFindById(){
         //Given
         Long id = 1L;
-        Ingredient ingredient = new Ingredient(1L, "G01-ML-0001I", "Ginebra", UnitOfMeasure.ML, null);
-        IngredientResponseDTO ingredientResponseDTO = new IngredientResponseDTO();
-        ingredientResponseDTO.setId(1L);
-        ingredientResponseDTO.setCode("G01-ML-0001I");
-        ingredientResponseDTO.setName("Ginebra");
-        ingredientResponseDTO.setUnitOfMeasure("ML");
-
-        when(this.ingredientRepository.findById( anyLong() )).thenReturn(Optional.of(ingredient));
-        when(modelMapper.map(ingredient, IngredientResponseDTO.class)).thenReturn(ingredientResponseDTO);
 
         //When
         IngredientResponseDTO result = ingredientService.findById(id);
@@ -78,7 +109,36 @@ class IngredientServiceImplTest {
         Assertions.assertEquals(UnitOfMeasure.ML.toString(), result.getUnitOfMeasure());
     }
 
+    @Test
+    public void testFindByCode(){
+        //Given
+        String code = "G01-ML-0001I";
+
+        //When
+        IngredientResponseDTO result = ingredientService.findByCode(code);
+
+        //Then
+        Assertions.assertEquals("Ginebra", result.getName());
+        Assertions.assertEquals("G01-ML-0001I", result.getCode());
+        Assertions.assertEquals(UnitOfMeasure.ML.toString(), result.getUnitOfMeasure());
+    }
+
+    @Test
+    public void testFindAll(){
+
+        //When
+
+        List<IngredientResponseDTO> result = ingredientService.findAll();
+
+        //Then
+        Assertions.assertEquals(3, result.size());
+        Assertions.assertEquals("Ginebra", result.get(0).getName());
+        Assertions.assertEquals("Tonica", result.get(1).getName());
+        Assertions.assertEquals("Unidad de Limon", result.get(2).getName());
 
 
+
+
+    }
 
 }
