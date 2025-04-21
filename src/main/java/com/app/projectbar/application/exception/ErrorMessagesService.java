47ab570a638972879311884e7afdbab2271c491d
Santiago <santiagovalenciarios@gmail.com>
2025-04-21 13:53:40 -0500

    feat: agregar validaciones para facturacióe ónes y excepcióersonalizada en capa application
    - Se valida que las órdenes a facturar no estén ya facturadas (validateIfOrderCanBeBilled, setOrdersAsReady)
    - Se asegura que solo se procesen órdenes existentes (getExistingOrderOrThrow)
    - Se crea el paquete exception en la capa application con la excepción personalizada OrdersAlreadyBilledException
    - Se deja preparado el manejo global de excepciones (pendiente de completar en GlobalExceptionHandler)

---
+++ src/main/java/com/app/projectbar/application/exception/ErrorMessagesService.java
@@ -0,0 +1,4 @@
+package com.app.projectbar.application.exception;
+
+public class ErrorMessagesService {
+}
---
+++ src/main/java/com/app/projectbar/application/exception/OrdersAlreadyBilledException.java
@@ -0,0 +1,7 @@
+package com.app.projectbar.application.exception;
+
+public class OrdersAlreadyBilledException extends RuntimeException {
+  public OrdersAlreadyBilledException(String message) {
+    super(message);
+  }
+}
