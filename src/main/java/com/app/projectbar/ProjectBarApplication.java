5063373af82878c738c0e200902538a42dd080f3
Yesika <yesikasimi@gmail.com>
2026-01-27 17:16:25 -0500

    Refactor ProjectBarApplication: remove unused BCryptPasswordEncoder and password encoding logic from main method

--- src/main/java/com/app/projectbar/ProjectBarApplication.java
+++ src/main/java/com/app/projectbar/ProjectBarApplication.java
@@ -2,7 +2,6 @@ package com.app.projectbar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(scanBasePackages = "com.app.projectbar")
 public class ProjectBarApplication {
@@ -10,14 +9,6 @@ public class ProjectBarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBarApplication.class, args);
 
-
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-		String adminPassword = encoder.encode("admin123");
-		String meseroPassword = encoder.encode("mesero123");
-
-		System.out.println("meseroPassword = " + meseroPassword);
-		System.out.println("adminPassword = " + adminPassword);
 	}
 
 
